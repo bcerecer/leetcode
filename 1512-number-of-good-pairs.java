d638e59ef3206ec44639c77f511ad20b66fd9108
nambawan <nambawan@Nambawans-MacBook-Pro.local>
2020-08-23 18:50:52 -0700

    1512 solution

--- 1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs.java
@@ -5,21 +5,26 @@ import java.util.Map;
  * @lc app=leetcode id=1512 lang=java
  *
  * [1512] Number of Good Pairs
+ * 
+ * Complexity:
+ *  Runtime: O(n)
+ *  Space: O(n)
  */
 
 // @lc code=start
 class Solution {
     public int numIdenticalPairs(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
-
-        Map<Integer, Integer> map = new HashMap<>();
         int counter = 0;
 
+        Map<Integer, Integer> map = new HashMap<>();
+
         for (int num : nums) {
-            int currentMatches = map.getOrDefault(num, 0);
-            counter += currentMatches;
-            map.put(num, currentMatches+1);
+            int occurrences = map.getOrDefault(num, 0);
+            counter += occurrences;
+            map.put(num, occurrences+1);
         }
+
         return counter;
     }
 }
